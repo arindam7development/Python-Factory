66888511d154f909207200acf330898c49ed0932
Arindam Mishra <arindam5191@gmail.com>
2015-04-15 11:45:43 +0530

    Create nested_list1.py

---
+++ nested_list1.py
@@ -0,0 +1,6 @@
+list = ["list1_item#1",10,"list1_item#2",20,"list1_item#3",30,"list1_item#4",40,"list1_item#5",50,
+       
+          ["list2_item#1","list2_item#2",["list3_item#1","list3_item#2"]]]
+
+for l in list:
+   print(l)
