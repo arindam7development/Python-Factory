b85eebbc201e1771c84a1b34ebcdd34f3533eb77
Arindam Mishra <arindam5191@gmail.com>
2015-04-15 12:22:37 +0530

    Create recursion_list.py

---
+++ recursion_list.py
@@ -0,0 +1,15 @@
+list = ["list1_item#1",10,"list1_item#2",20,"list1_item#3",30,"list1_item#4",40,"list1_item#5",50,
+       
+          ["list2_item#1","list2_item#2",["list3_item#1","list3_item#2"]]]
+          
+          
+def print_lol(the_list):
+   for each_item in the_list:
+        if isinstance(each_item, list):
+             print_lol(each_item)
+        else:
+             print(each_item)
+
+
+
+print_lol(list)
