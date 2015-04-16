22b71e67e78eee43acb2844aa1313c66e76e0718
Arindam Mishra <arindam5191@gmail.com>
2015-04-16 12:20:13 +0530

    Update nested_list2.py

--- nested_list2.py
+++ nested_list2.py
@@ -1,4 +1,7 @@
 list = ["list1_item#1",10,"list1_item#2",20,"list1_item#3",30,"list1_item#4",40,"list1_item#5",50,
        
           ["list2_item#1","list2_item#2",["list3_item#1","list3_item#2"]]]
-print(list[10][2][0])
+#print(list[10][2][0])
+
+for a in list:
+   print(a)
