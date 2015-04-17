0908a5a6ae3cce2cd6d4f2b631ef8e2665e641fb
Arindam Mishra <arindam5191@gmail.com>
2015-04-17 12:03:51 +0530

    Update recursion_list.py

--- recursion_list.py
+++ recursion_list.py
@@ -2,7 +2,7 @@ list = ["list1_item#1",10,"list1_item#2",20,"list1_item#3",30,"list1_item#4",40,
        
           ["list2_item#1","list2_item#2",["list3_item#1","list3_item#2"]]]
           
-          
+# Use of Methods in Python      
 def print_lol(the_list):
    for each_item in the_list:
         if isinstance(each_item, list):
